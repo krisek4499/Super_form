6180772672df4e97b557610b777b00055abe5246
krisek4499 <52441938+krisek4499@users.noreply.github.com>
2019-07-29 11:07:49 +0200

    changes

--- emailforms.php
+++ emailforms.php
@@ -5,21 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Formularz</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="apsss.css" rel="stylesheet">
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">-->
 	  <!--<script src="js/jquery-1.11.1.min.js"><script>-->
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  </head>
-   <body>
+ </head>
+ <body>
+    <div class="top"> 
+
+    </div>
+    <div class="TRESC">
+      <div class="navbar-nav">
+        <div class="list-group">
+         <a href="#" class="list-group-item list-group-item-action active">Wyświetl użytkowników</a>
+         <a href="#" class="list-group-item list-group-item-action">Edytuj użytkowników</a>
+         <a href="#" class="list-group-item list-group-item-action">Usuń użytkowników</a>
+         <a href="#" class="list-group-item list-group-item-action">Formularz</a>
+        </div>
+      </div>
+    </div>
      <div class="container">
-       <div class="row">
-         <div class="col-sm-12">
-           <form action="index.html" method="post">
-             <input type="submit" class="btn btn-success" value="powrót do formularza" />
+        <div class="row" >
+         <div class="col-sm-4 col-md-4 ">
+            <form action="index.html" method="post">
+           <input type="submit" class="btn btn-success" value="powrót do formularza" />
            </form>
-         </div>   
-       </div>
-     </div>
-   </body>
+         </div>
+         <div class="col-sm-4 col-md-4">
+      
+          </div> 
+         <div class="col-sm-4 col-md-4">
+
+          </div>    
+        </div>
+      </div>
+    <div class="STOPKA"> </div>
+  </body>
 </html>
